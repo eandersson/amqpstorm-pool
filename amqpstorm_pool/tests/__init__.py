b437704ad242d8b4020fbb51567ee7ec41a8494b
Erik Olof Gunnar Andersson <me@eandersson.net>
2020-06-26 20:06:40 -0700

    Improved test coverage

--- amqpstorm_pool/tests/__init__.py
+++ amqpstorm_pool/tests/__init__.py
@@ -1,6 +1,10 @@
 import json
 import time
-import unittest
+
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
 
 import amqpstorm
 from amqpstorm.tests import utility
@@ -20,8 +24,7 @@ def create_fake_connection():
     return connection
 
 
-class PoolTests(unittest.TestCase):
-
+class QueuedPoolTests(unittest.TestCase):
     def test_simple_publish(self):
         pool = amqpstorm_pool.QueuedPool(
             create=lambda: create_fake_connection(),
@@ -49,6 +52,23 @@ class PoolTests(unittest.TestCase):
             )
 
         self.assertEqual(1, pool._queue.qsize())
+
+    def test_pool_fairy_representation(self):
+        pool = amqpstorm_pool.QueuedPool(
+            create=lambda: create_fake_connection(),
+            max_size=10,
+            max_overflow=10,
+            timeout=10,
+            recycle=3600,
+            stale=45,
+        )
+
+        with pool.acquire() as cxn:
+            self.assertRegexpMatches(
+                str(cxn.fairy),
+                'cxn=localhost:1234/None, channel=None, '
+                'created_at=.*, released_at=.*'
+            )
 
     def test_create_two_connections(self):
         pool = amqpstorm_pool.QueuedPool(
@@ -98,16 +118,43 @@ class PoolTests(unittest.TestCase):
 
         self.assertEqual(0, pool._queue.qsize())
 
-        with pool.acquire():
-            with pool.acquire():
-                pass
+        with pool.acquire() as cxn1:
+            self.assertIsNotNone(cxn1.channel)
+            with pool.acquire() as cxn2:
+                self.assertIsNotNone(cxn2.channel)
 
         self.assertEqual(2, pool._queue.qsize())
 
         time.sleep(0.125)
 
-        with pool.acquire():
-            pass
+        with pool.acquire() as cxn:
+            self.assertIsNotNone(cxn)
+
+        self.assertEqual(1, pool._queue.qsize())
+
+    def test_queue_stale(self):
+        pool = amqpstorm_pool.QueuedPool(
+            create=lambda: create_fake_connection(),
+            max_size=10,
+            max_overflow=0,
+            timeout=0.1,
+            recycle=45,
+            stale=0.1,
+        )
+
+        self.assertEqual(0, pool._queue.qsize())
+
+        with pool.acquire() as cxn1:
+            self.assertIsNotNone(cxn1.channel)
+            with pool.acquire() as cxn2:
+                self.assertIsNotNone(cxn2.channel)
+
+        self.assertEqual(2, pool._queue.qsize())
+
+        time.sleep(0.125)
+
+        with pool.acquire() as cxn:
+            self.assertIsNotNone(cxn)
 
         self.assertEqual(1, pool._queue.qsize())
 
@@ -125,5 +172,25 @@ class PoolTests(unittest.TestCase):
 
         with pool.acquire():
             self.assertRaises(amqpstorm_pool.Timeout, pool.acquire)
+
+        self.assertEqual(1, pool._queue.qsize())
+
+    def test_queue_with_closed_connection(self):
+        pool = amqpstorm_pool.QueuedPool(
+            create=lambda: create_fake_connection(),
+            max_size=1,
+            max_overflow=1,
+            timeout=10,
+            recycle=3600,
+            stale=45,
+        )
+
+        self.assertEqual(0, pool._queue.qsize())
+
+        with pool.acquire() as cxn:
+            cxn.fairy.close()
+
+        with pool.acquire():
+            pass
 
         self.assertEqual(1, pool._queue.qsize())
